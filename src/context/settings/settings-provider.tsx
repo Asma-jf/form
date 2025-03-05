13c4df2f516c9a9792d04d523340a5ce93faeaf1
unknown <jafariasma1372@gmail.com>
2025-03-05 22:32:36 +0330

    create themeswitcher again

--- src/context/settings/settings-provider.tsx
+++ src/context/settings/settings-provider.tsx
@@ -1,24 +1,20 @@
 import { SettingsValueProps } from "@/context/settings/types";
-import { SettingsContext } from "./settings-context";
+import { SettingsContext } from './settings-context';
 import React, { useMemo } from "react";
-import { ThemeProvider } from "next-themes";
 
 type SettingsProviderProps = {
-  children: React.ReactNode;
-  defaultSettings: SettingsValueProps;
+    children: React.ReactNode;
+    defaultSettings: SettingsValueProps;
 };
 
 export function SettingsProvider({ children, defaultSettings }: SettingsProviderProps) {
-  const memoizedValue = useMemo(
-    () => ({
-      ...defaultSettings,
-    }),
-    [defaultSettings]
-  );
 
-  return (
-    <SettingsContext.Provider value={memoizedValue}>
-      <ThemeProvider attribute="class">{children}</ThemeProvider>
-    </SettingsContext.Provider>
-  );
+    const memoizedValue = useMemo(
+        () => ({
+s
+        }),
+        []
+    );
+
+    return <SettingsContext.Provider value={memoizedValue}>{children}</SettingsContext.Provider>;
 }
